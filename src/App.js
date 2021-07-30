dce60747f6440c4d55b98b20eeea3c43bf39de61
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-07-30 13:32:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 className="text-4xl text-white bg-black">
+          Hello world
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.css";
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    mountNode);
