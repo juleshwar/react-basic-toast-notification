f3c9f8026d02c34c8c547f92db761e49d180a271
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-01 23:16:19 +0530

    feat: adding default state of notifications
    also adding a handler to show the notifications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
-
+import NotificationHandler from './NotificationHandler';
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: [],
+      notificationCounter: 0
+    }
+  }
   render() {
     function onClickTriggerNotification() {
       // trigger a notification
@@ -13,6 +20,7 @@ class App extends React.Component {
         <div className="py-5 flex justify-around">
           <button className="bg-gray px-4 py-2 border rounded" onClick={onClickTriggerNotification}>Trigger Notification</button>
           <button className="bg-gray px-4 py-2 border rounded" onClick={onClickClearNotifications}>Clear All Notifications</button>
+          <NotificationHandler notifications={notifications} />
         </div>
       </>
     );
