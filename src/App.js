276e12584d7b5fd23787b7f992041e226d31817a
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-01 23:34:20 +0530

    fix: moving functions out of render function
    also binding them to the current class

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ class App extends React.Component {
       notifications: [],
       notificationCounter: 0
     }
+    this.onClickTriggerNotification = this.onClickTriggerNotification.bind(this);
+    this.onClickClearNotifications = this.onClickClearNotifications.bind(this);
+  }
+  onClickTriggerNotification() {
+    // trigger a notification
+  }
+  onClickClearNotifications() {
+    // clear all notifications
   }
   render() {
-    function onClickTriggerNotification() {
-      // trigger a notification
-    }
-    function onClickClearNotifications() {
-      // clear all notifications
-    }
     return (
       <>
         <div className="py-5 flex justify-around">
-          <button className="bg-gray px-4 py-2 border rounded" onClick={onClickTriggerNotification}>Trigger Notification</button>
-          <button className="bg-gray px-4 py-2 border rounded" onClick={onClickClearNotifications}>Clear All Notifications</button>
-          <NotificationHandler notifications={notifications} />
+          <button className="bg-gray px-4 py-2 border rounded" onClick={this.onClickTriggerNotification}>Trigger Notification</button>
+          <button className="bg-gray px-4 py-2 border rounded" onClick={this.onClickClearNotifications}>Clear All Notifications</button>
+          <NotificationHandler notifications={this.state.notifications} />
         </div>
       </>
     );
