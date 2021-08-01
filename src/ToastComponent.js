9c6b50e93e6f4d038dd0fbd9f8a65583dd72774e
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-01 22:45:56 +0530

    feat: adding basic structure for toast component

---
+++ src/ToastComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ToastComponent(props) {
+    const {title, message} = props
+    return (
+        <div className="flex flex-col items-center bg-red-900 px-2 py-4">
+            <h2>{title}</h2>
+            <p className="mt-2">{message}</p>
+        </div>
+    )
+}
+
+export default ToastComponent
