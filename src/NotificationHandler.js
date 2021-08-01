3c99b45b4836d8c28f8de1169e8ae24e66456633
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-02 00:05:59 +0530

    feat: modifying NotificationHandler to create a portal
    creating a portal to a div inside #app and showing notifcations there

--- src/NotificationHandler.js
+++ src/NotificationHandler.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import ToastComponent from './ToastComponent'
 
-function NotificationHandler(props) {
-    const { notifications } = props
-    return (
-        <div>
-            {notifications.map((notification) => {
-                const { title, message } = notification
-                return (
-                    <div key={notification.id}>
-                        <ToastComponent title={title} message={message} />
-                    </div>
-                )
-            })}
-        </div>
-    )
+export default class NotificationHandler extends React.Component {
+    constructor(props) {
+        super(props)
+        this.setupNotificationContainer()
+    }
+    componentDidMount() {
+        document.getElementById("app").appendChild(this.notificationContainer)
+    }
+    componentWillUnmount() {
+        document.getElementById("app").removeChild(this.notificationContainer)
+    }
+    setupNotificationContainer() {
+        const notificationContainer = document.createElement('div')
+        notificationContainer.classList.add("fixed", "z-10", "h-full", "w-full", "top-0", "left-0")
+        notificationContainer.id = 'notifications-container'
+        this.notificationContainer = notificationContainer
+    }
+    render() {
+        return ReactDOM.createPortal(
+            <>
+                {
+                    this.props.notifications.map((notification) => {
+                        const { title, message } = notification
+                        return (
+                            <div key={notification.id}>
+                                <ToastComponent title={title} message={message} />
+                            </div>
+                        )
+                    })
+                }
+            </>,
+            this.notificationContainer
+        )
+    }
 }
-
-export default NotificationHandler
