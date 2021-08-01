2b97c82083766898af537ee6c5b9af2ed26cf3f5
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-01 22:51:02 +0530

    feat: adding UI action buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   render() {
+    function onClickTriggerNotification() {
+      // trigger a notification
+    }
+    function onClickClearNotifications() {
+      // clear all notifications
+    }
     return (
       <>
-        <h1 className="text-4xl text-white bg-black">
-          Hello world
-        </h1>
+        <div className="py-5 flex justify-around">
+          <button className="bg-gray px-4 py-2 border rounded" onClick={onClickTriggerNotification}>Trigger Notification</button>
+          <button className="bg-gray px-4 py-2 border rounded" onClick={onClickClearNotifications}>Clear All Notifications</button>
+        </div>
       </>
     );
   }
