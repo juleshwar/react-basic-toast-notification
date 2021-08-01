166fd9636f40fc1f88bb79a6ee5137efcb64d337
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-02 00:10:45 +0530

    refactor: adding fixed width for toast

--- src/ToastComponent.js
+++ src/ToastComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ToastComponent(props) {
     const {title, message} = props
     return (
-        <div className="flex flex-col items-center bg-red-900 px-2 py-4">
+        <div className="flex flex-col items-center bg-red-900 px-2 py-4 w-64">
             <h2>{title}</h2>
             <p className="mt-2">{message}</p>
         </div>
