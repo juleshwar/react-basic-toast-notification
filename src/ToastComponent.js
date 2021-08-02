1fbe277dde5f4a1223615c9657a8edf7203adac3
juleshwar.babu <juleshwar.babu@zohocorp.com>
2021-08-02 14:00:15 +0530

    style: making text white for better contrast

--- src/ToastComponent.js
+++ src/ToastComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ToastComponent(props) {
     const {title, message, id, onClickClose} = props
     return (
-        <div className="flex flex-col relative items-center bg-red-900 px-2 py-4 w-64">
+        <div className="flex flex-col relative items-center bg-red-900 px-2 py-4 w-64 text-white">
             <button className="absolute right-2 top-2 transform rotate-45 text-2xl" onClick={onClickClose.bind(this, id)}>+</button>
             <h2>{title}</h2>
             <p className="mt-2">{message}</p>
